86bf1c8c4a3a094932f135bb22ac8bfd59bb79bf
Jian Zhong <zhongjian089@gmail.com>
2022-07-21 03:20:32 -0700

    improve readability of HW1

--- HW1/Object_1/server.py
+++ HW1/Object_1/server.py
@@ -1,16 +1,14 @@
 import socketserver
 import sys
-import time
-
 
 # MyTCPHandler is also a base handler inherited from BaseRequestHandler
 class MyTCPHandler(socketserver.BaseRequestHandler):
     clients = [] # if you want some data persist through all connections, use it
 
-        # get request body if exists, return None if no body content
-    def getRequestBodyByLength(self, headerDict, request_body):
+    # get request body if exists, return None if no body content
+    def getRequestBodyByLength(self, headerDict, request_content):
         if "Content-Length" in headerDict:
-            return( request_body[:int(headerDict["Content-Length"])] )
+            return( request_content[:int(headerDict["Content-Length"])] )
         else:
             return None
             
@@ -40,27 +38,39 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         received_data = self.request.recv(1024)
 
         # 127.0.0.1(host) : 51693(client port number)
-        client_id = self.client_address[0] + ":" + str(self.client_address[1]) 
-        print(client_id + " is sending data:") 
-        print(len(received_data)) # length of client request content
-        self.clients.append(client_id)
-        print(self.clients)
+        client_id = self.client_address[0] + ":" + str(self.client_address[1]) # 127.0.0.1:65413
+        print("Client: " + client_id + " is requesting data")            # 127.0.0.1:65413 is sending data:
+        print("Data received length: " + str(len(received_data)) )            # length of client request content
         
         # Prepare HTTP request data
-        HTTP_request = received_data.decode()  # get decoded HTTP request
-        request_list = HTTP_request.split("\r\n\r\n")    # split by double new line signal
-        request_but_body = request_list[0]  # un-parsed request data except body
-        request_body = request_list[1]  # un-parsed request body 
+        HTTP_request = received_data.decode()            # get decoded HTTP request
+        print("**************** Http Request: ****************")
+        print(HTTP_request)
+        print("***********************************************")
+        request_list = HTTP_request.split("\r\n\r\n")    # split HTTP request into array by double new line symbol
+        request_line = request_list[0]                   # HTTP request line: GET / HTTP/1.1
+        request_body = request_list[1]                   # request body
+        print("**************** HTTP request line ****************")
+        print( str(request_line) )
+        print("**************************************************")
+        print("**************** HTTP request body ****************")
+        print( str(request_body) )
+        print("**************************************************")
 
         # Parse and get HTTP request line
-        request_data_list = request_but_body.split("\r\n")   # split the request data by single new line signal
-        method_path_ver = request_data_list[0].split(" ") # get the first request line list: [method, path, HTTPver]
+        print("****************  Parsing HTTP request ****************")
+        data_list = request_line.split("\r\n")        # split the request data by single new line symbol
+        print("HTTP Data list: " + str(data_list) )
 
         # Parse and get HTTP header key-value pairs
-        del request_data_list[0]     # get rid of the first request line list, the rest is header string
-        headerDict = self.getHeader(request_data_list) # parse and get the request header key-value pairs
+        method_path_ver = data_list[0].split(" ")     # turn HTTP request line into: [method, path, HTTP ver]
+        del data_list[0]     # get rid of the first request line list, the rest are all header pairs arrays
+        headerDict = self.getHeader(data_list) # parse and get the request header key-value pairs
+        print("HeaderDict: " + str(headerDict) )
+        print("*******************************************************")
+
         # Parse and get HTTP request body, if any
-        body = self.getRequestBodyByLength(headerDict, request_body) # request body string of length specified by request header
+        request_content = self.getRequestBodyByLength(headerDict, request_body) # request body string of length specified by request header
 
         # Parse the requested path
         method = method_path_ver[0]
@@ -89,6 +99,12 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         print("\n\n")
         sys.stdout.flush() # needed to use combine with docker
         sys.stderr.flush() # whatever you have buffer, print it out to the screen
+
+        print("Constructing HTTP Response...")
+        print("HTTP requested path is: " + path)
+        print("----------- HTTP response --------------")
+        print(resp)
+        print("----------------------------------------")
         self.request.sendall(resp.encode()) # Send data here!!
 
 
@@ -96,6 +112,5 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
 if __name__ == "__main__":
     host = "localhost"
     port = 8080
-
     server = socketserver.ThreadingTCPServer((host, port), MyTCPHandler)
     server.serve_forever() 
